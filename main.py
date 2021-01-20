a7c6d4075805389a38e74920afa05604750761b4
Heimer Rojas <ing.heimer.rojas@gmail.com>
2021-01-20 01:11:25 +0000

    schedule function added

---
+++ main.py
@@ -0,0 +1,98 @@
+from flask import Flask
+from flask import render_template, request, url_for, make_response
+from werkzeug.utils import secure_filename
+import os
+from scripts.instance import init_fun, detect_img
+from flask import jsonify
+import re
+from werkzeug.datastructures import FileStorage
+import requests
+import shutil
+
+from google.cloud import storage
+from io import BytesIO
+from PIL import Image
+
+app = Flask(__name__)
+yolo_ins = init_fun(app.root_path)
+
+#import pdb; pdb.set_trace()
+
+CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET'] # for deploy
+# CLOUD_STORAGE_BUCKET = 'yolo_data' # for local test
+
+
+@app.route('/status')
+def status():
+    # import pdb; pdb.set_trace()
+    if request.headers.get("Content-Type") == 'application/json':
+        return jsonify(status="OK")
+    return 'OK'
+
+@app.route('/')
+def index(name=None):
+    frame = url_for('static', filename='images/frame.jpg')
+    return render_template('index.html',
+                            name=name,
+                            path_upload_image=frame,
+                            path_output_image=frame,
+                            label="Result")
+
+@app.route('/detection', methods=['POST'])
+def detection(name=None):
+    # import pdb; pdb.set_trace()
+
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+    if request.headers.get("Content-Type") == 'application/json':
+        request_json = request.get_json()
+        file_path = request_json.get("image")
+        name = re.split(r"/|\\", file_path)[-1]
+        response = requests.get(file_path)
+        image = Image.open(BytesIO(response.content))
+
+        # Creating the "string" object to use upload_from_string
+        img_byte_array = BytesIO()
+        image.save(img_byte_array, format='JPEG')
+
+        # Create a new blob and upload the file's content.
+        blob = bucket.blob(name)
+
+        blob.upload_from_string(img_byte_array.getvalue(),
+                                content_type="image/jpeg")
+        # The public URL can be used to directly access
+        # the uploaded file via HTTP.
+        uploaded_url = blob.public_url
+
+
+    else:
+        uploaded_file = request.files.get('file')
+        # Create a new blob and upload the file's content.
+        blob = bucket.blob(uploaded_file.filename)
+
+        blob.upload_from_string(
+                uploaded_file.read(),
+                content_type=uploaded_file.content_type
+        )
+
+        # The public URL can be used to directly access
+        # the uploaded file via HTTP.
+        uploaded_url = blob.public_url
+        name = uploaded_file.filename
+
+    output_url = detect_img(yolo_ins, uploaded_url, name)
+    if request.headers.get("Content-Type") == 'application/json':
+        return jsonify(output=output_url)
+    response = make_response(render_template('index.html',
+                            name='',
+                            path_upload_image=uploaded_url,
+                            path_output_image=output_url,
+                            label="Output image"))
+    return response
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True) # for deploy
+    # app.run(host='0.0.0.0', port='5000', debug=True) # for local test
---
+++ schedule.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import pandas as pd
+import numpy as np
+
+
+def appointment2(slot_data):
+    # companies = slot_data.iloc[:, 3:10].values.tolist()
+    # print(companies)
+    s = slot_data.shape
+    # print(s)
+    # print(slot_data.head())
+    companies = []
+    for i in range(0, s[0]):
+        for j in range(3, 10):
+            val = slot_data.iloc[i][j]
+            # print(val)
+            # print(slot_data.iloc[i, 0])
+            if not val in companies and type(val) == str:
+                companies.append(val)
+    print(companies)
+    for c in companies:
+        for i in range(s[0]):
+            if c in slot_data.iloc[i, 3:10].values.tolist():
+                if slot_data.iloc[i][2] == 'AM':
+                    j = 10
+                else:
+                    j = 20
+                space = slot_data.iloc[i, j]
+                print("space: ", space)
+                check_list = slot_data.iloc[:i, j].values.tolist()
+                while type(space) == str or c in check_list:
+                    j += 1
+                    space = slot_data.iloc[i, j]
+                    check_list = slot_data.iloc[:i, j].values.tolist()
+                print("value: ", c, "position i: ", i, "position j: ", j, "current value: ", slot_data.iloc[i, j])
+                # print(check_list)
+                slot_data.iloc[i, j] = c
+                print(slot_data.iloc[:,20:30])
+
+
+    return slot_data
+
+
+
+pd.set_option('display.max_columns', None)
+data = pd.read_csv('/content/drive/MyDrive/Techstars Challenge - Source Data.csv')
+s = data.shape
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+slots = ['AM', 'PM']
+am_hours = ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.', '9:00 a.m.', '9:20 a.m.', '9:40 a.m.', '10:00 a.m.', '10:20 a.m.', '10:40 a.m.', '11:00 a.m.']
+pm_hours = ['2:00 p.m.', '2:20 p.m.', '2:40 p.m.', '3:00 p.m.', '3:20 p.m.', '3:40 p.m.', '4:00 p.m.', '4:20 p.m.', '4:40 p.m.', '5:00 p.m.']
+
+for col in am_hours:
+    data[col] = np.nan
+for col in pm_hours:
+    data[col] = np.nan
+
+m = data.shape
+# day = data.iloc[0][1]
+# slot = data.iloc[0][2]
+# print(day, slot)
+list_data = []
+for day in days:
+    for slot in slots:
+        print(day)
+        print(slot)
+        # j = i
+        # while data.iloc[i][1] == day and data.iloc[i][2] == slot:
+        slot_data = data[(data['Day'] == day) & (data['AM/PM'] == slot)]
+        # print(slot_data.head())
+        sorted_slot_data = appointment2(slot_data)
+        print(sorted_slot_data)
+        sorted_slot_data.to_csv('schedule_{}_{}.csv'.format(day, slot), index=False)
+        list_data.append(sorted_slot_data)
+data_concat = pd.concat(list_data)
+data_concat.to_csv('schedule_from_list.csv', index=False)
